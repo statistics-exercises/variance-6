19f366a19d62ba6a02e732c8ad9f3e1c308635b6
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-29 19:46:43 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# The data is read into a numpy array called radii
+radii = np.loadtxt("bubbles.dat")
+
+# To complete the exercise you need to use the instructions on the 
+# the variance from the input data.
+S2 = 0
+
+
+print("The variance for the radii of the bubbles is", S2)
