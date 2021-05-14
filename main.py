fb65252d493794740830c1bd58195c3f3a276c0b
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-14 14:39:09 +0100

    Added answer

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import numpy as np
 
 def variance(n) :
   # Your function to calculate the variance for a set of n uniform random variables goes here
-
+  myrand = np.random.uniform(0,1)
+  S, S2 = myrand, myrand*myrand 
+  for i in range(n-1) : 
+      myrand = np.random.uniform(0,1)
+      S, S2 = S + myrand, S2 + myrand*myrand 
+  S = S / n 
+  return ( n / (n-1) )*( S2 / n - S*S )
